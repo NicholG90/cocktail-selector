7745a97b0601fb804a9c1230b33f8672447c888f
NicholG90 <41203930+NicholG90@users.noreply.github.com>
2020-05-24 23:22:07 -0400

    Add files via upload

---
+++ precache-manifest.c067405503546667d1d96af491c16ecf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a89762281b089c6feff2e663cace44",
+    "url": "/cocktail-selector/index.html"
+  },
+  {
+    "revision": "ea5df8250bc985e9b820",
+    "url": "/cocktail-selector/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "1c11c3fa0c494425c6c5",
+    "url": "/cocktail-selector/static/css/main.7e9b28ac.chunk.css"
+  },
+  {
+    "revision": "ea5df8250bc985e9b820",
+    "url": "/cocktail-selector/static/js/2.2f42c6a3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cocktail-selector/static/js/2.2f42c6a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c11c3fa0c494425c6c5",
+    "url": "/cocktail-selector/static/js/main.ca141eb7.chunk.js"
+  },
+  {
+    "revision": "399209f7161f9cda5a9a",
+    "url": "/cocktail-selector/static/js/runtime-main.24f0702d.js"
+  },
+  {
+    "revision": "b7fdbb02612771ff3c6fab19d8451ca2",
+    "url": "/cocktail-selector/static/media/cocktail.b7fdbb02.svg"
+  }
+]);
